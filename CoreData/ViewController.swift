6553a185a1673b286dd58f9ba1de6dc2a8aa7c0d
lama23 <florenz.erstling@lalalama.de>
2016-12-23 15:16:55 +0100

    initial push

--- CoreData/ViewController.swift
+++ CoreData/ViewController.swift
@@ -7,8 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtDesc: UITextField!
+    
+    @IBOutlet weak var txtImg: UITextField!
+    @IBOutlet weak var txtLong: UITextField!
+    @IBOutlet weak var txtLang: UITextField!
+    
+    let moContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        
+    func saveData(){
+        
+        // Get Description
+        //let storeDescription = NSEntityDescription.entityForName("Store", inManagedObjectContext: moContext!)
+
+        //let store = Store(entity: storeDescription!, insertIntoManagedObjectContext: moContext)
+        let newContact = NSEntityDescription.insertNewObject(forEntityName: "Store", into: moContext)
+        
+        newContact.setValue(txtName.text, forKey: "sName")
+        newContact.setValue(txtDesc.text, forKey: "sDesc")
+        newContact.setValue(txtImg.text, forKey: "sImg")
+        newContact.setValue(txtLong.text, forKey: "sLang")
+        newContact.setValue(txtLang.text, forKey: "sLat")
+                
+        do {
+            try moContext.save()
+            print("saved")
+        }
+        catch {
+            print("nein")
+        }
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
